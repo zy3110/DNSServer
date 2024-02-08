3c69cf46d9185637dee4d8c2f2b92ed1a28d8167
yang5313876111 <158246318+yang5313876111@users.noreply.github.com>
2024-02-08 21:13:47 +0800

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -75,6 +75,25 @@ dns_records = {
             86400, #minimum
         ),
     },
+    'safebank.com.': {
+        dns.rdatatype.A: '192.168.1.102',
+    },
+    'google.com.': {
+        dns.rdatatype.A: '192.168.1.103',
+    },
+    'legitsite.com.': {
+        dns.rdatatype.A: '192.168.1.104',
+    },
+    'yahoo.com.': {
+        dns.rdatatype.A: '192.168.1.105',
+    },
+    'nyu.edu.': {
+        dns.rdatatype.A: '192.168.1.106',
+        dns.rdatatype.TXT: (encrypted_value,),
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        dns.rdatatype.NS: 'ns1.nyu.edu.',
+    }
    
     # Add more records as needed (see assignment instructions!
 }
