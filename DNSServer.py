5cd8e38cd816623076faa09686b4fed708d58476
yang5313876111 <158246318+yang5313876111@users.noreply.github.com>
2024-02-08 21:16:20 +0800

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -89,7 +89,7 @@ dns_records = {
     },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: (encrypted_value,),
+        dns.rdatatype.TXT: (str(encrypted_value),),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
